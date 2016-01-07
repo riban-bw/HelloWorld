5a89d28fa4cdd0c6991bcf2855663d13a79773ac
Walt <brian@riban.co.uk>
2016-01-07 19:25:04 +0000

    w

--- publish.sh
+++ publish.sh
@@ -16,10 +16,9 @@ git clone -b gh-pages "${REPO_PATH}" --single-branch ${HTML_PATH}
 cd ${HTML_PATH}
 
 # Check if this revision has already been documented by another (matrix) build
-echo "Checking for $CHANGESET in previous commit messages"
-COMMIT_MSG="Automated API documentation build for changeset ${CHANGESET}."
-LAST_MSG=`git log -n 1`
-[ "$COMMIT_MSG" = "$LAST_MSG" ] && exit 0
+echo "Build ID: $TRAVIS_BUILD_ID"
+echo "Build number: $TRAVIS_BUILD_NUMBER"
+echo "Job ID: $TRAVIS_JOB_ID"
 
 echo "<html><head><title>Test page</title></head><body>1. This is build $CHANGESET.</body></html>" > index.html
 echo "<html><body>Version $VERSION</body><html>" > "$VERSION.html"
