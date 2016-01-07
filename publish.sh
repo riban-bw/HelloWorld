e37f19324fe3a16156b4f6ec20ad11d6081aedc3
Walt <brian@riban.co.uk>
2016-01-07 20:08:27 +0000

    Put once only in travis

--- publish.sh
+++ publish.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -e
 
 # Only run on first Travis-ci job to avoid running for each platform / version
-JOB="${TRAVIS_JOB_NUMBER: -1}"
-if [ "$JOB.0" != "1.0" ]
-then
-  return 0
-fi
+#JOB="${TRAVIS_JOB_NUMBER: -1}"
+#if [ "$JOB.0" != "1.0" ]
+#then
+#  return 0
+#fi
 
 # Settings
 REPO_PATH=git@github.com:riban-bw/HelloWorld.git
