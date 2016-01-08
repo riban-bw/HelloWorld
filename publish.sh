3e61742fb2bd3d3746e47cbc5e171151fb111c4a
Walt <brian@riban.co.uk>
2016-01-08 15:07:23 +0000

    update publish

--- publish.sh
+++ publish.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -e
+# Passing '-e' parameter to bash to exit script immediately if any command fails
 
 # Only run on first Travis-ci job to avoid running for each platform / version
 if [ "${TRAVIS_JOB_NUMBER: -1}" != "1" ]
@@ -29,15 +30,15 @@ ls
 echo "doxygen complete"
 NOT_DOCED=`grep "is not documented" error.log | wc -l`
 NOT_DOC_MEMBER=`grep "Member.*is not documented" error.log | wc -l`
-NOT_DOC_PARAM=`grep "The following parameters of .* are not documented" | wc -l`
+NOT_DOC_PARAM=`grep "The following parameters of .* are not documented" error.log | wc -l`
 echo "Getting undocumented parameters"
-#DOC_PARAM=`grep "The following parameters of .* are not documented" doxyerror.log | awk --field-separator " of " '{ print $2 }' | awk --field-separator " are not documented" '{ print $1"<br/>" }'`
+DOC_PARAM=`grep "The following parameters of .* are not documented" error.log | awk --field-separator " of " '{ print $2 }' | awk --field-separator " are not documented" '{ print $1"<br/>" }'`
 echo "Getting incorrect arguments"
-#DOC_ERROR=`grep "is not found in the argument list" error.log`
+DOC_ERROR=`grep "is not found in the argument list" error.log`
 echo "Getting unsuppported tags"
-#DOC_UNSUPPORTED=`grep "Unsupported xml/html tag"`
+DOC_UNSUPPORTED=`grep "Unsupported xml/html tag" error.log`
 
-#echo "<html><body>There are $NOT_DOCED elements not yet documented<br/><br/>There are $NOT_DOC_MEMBER undocumented member elements.<br/>Thefollowing functions have undocumented parameters:<br/>$DOC_PARAM</br>The following errors in dcumentation require fixing:<br/>$DOC_ERROR<br/><br/>$DOC_UNSUPPORTED</body></html>" > ${HTML_PATH}/api/report.html
+echo "<html><body>There are $NOT_DOCED elements not yet documented<br/><br/>There are $NOT_DOC_MEMBER undocumented member elements.<br/>Thefollowing functions have undocumented parameters:<br/>$DOC_PARAM</br>The following errors in dcumentation require fixing:<br/>$DOC_ERROR<br/><br/>$DOC_UNSUPPORTED</body></html>" > ${HTML_PATH}/api/report.html
 
 # Create and commit the documentation repo.
 cd ${HTML_PATH}
