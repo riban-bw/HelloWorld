8a04184742e7eaf78432dd761d8004367d9d9f55
Walt <brian@riban.co.uk>
2016-01-08 14:37:16 +0000

    Adds some docs

--- hello.c
+++ hello.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 
+/**	@defgroup fn Functions and stuff
+*	@{
+*/
+
+/**	Adds two numbers together
+*	@param	int First number
+*	@retval int Result
+*/
+int Add(int n1, int n2)
+{
+  return n1 + n2;
+}
+
+/**	This is the main function
+*	@param	BadParam This parameter does not exist
+*/
 int main()
 {
   printf("Hello world!\r\n");
 }
 
+/**	@} */
