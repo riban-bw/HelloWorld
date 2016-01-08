bf6a64b08171ec3026006bc7d43ba290bbbbdabe
Walt <brian@riban.co.uk>
2016-01-08 14:44:04 +0000

    2

--- publish.sh
+++ publish.sh
@@ -27,17 +27,17 @@ doxygen >doxygen.log 2>error.log
 
 ls
 echo "doxygen complete"
-#NOT_DOCED=`grep "is not documented" error.log | wc -l`
-#NOT_DOC_MEMBER=`grep "Member.*is not documented" error.log | wc -l`
-#NOT_DOC_PARAM=`grep "The following parameters of .* are not documented" | wc -l`
+NOT_DOCED=`grep "is not documented" error.log | wc -l`
+NOT_DOC_MEMBER=`grep "Member.*is not documented" error.log | wc -l`
+NOT_DOC_PARAM=`grep "The following parameters of .* are not documented" | wc -l`
 echo "Getting undocumented parameters"
-#DOC_PARAM=`grep "The following parameters of .* are not documented" doxyerror.log | awk --field-separator " of " '{ print $2 }' | awk --field-separator " are not documented" '{ print $1"<br/>" }'`
+DOC_PARAM=`grep "The following parameters of .* are not documented" doxyerror.log | awk --field-separator " of " '{ print $2 }' | awk --field-separator " are not documented" '{ print $1"<br/>" }'`
 echo "Getting incorrect arguments"
-#DOC_ERROR=`grep "is not found in the argument list" error.log`
+DOC_ERROR=`grep "is not found in the argument list" error.log`
 echo "Getting unsuppported tags"
-#DOC_UNSUPPORTED=`grep "Unsupported xml/html tag"`
+DOC_UNSUPPORTED=`grep "Unsupported xml/html tag"`
 
-#echo "<html><body>There are $NOT_DOCED elements not yet documented<br/><br/>There are $NOT_DOC_MEMBER undocumented member elements.<br/>Thefollowing functions have undocumented parameters:<br/>$DOC_PARAM</br>The following errors in dcumentation require fixing:<br/>$DOC_ERROR<br/><br/>$DOC_UNSUPPORTED</body></html>" > ${HTML_PATH}/api/report.html
+echo "<html><body>There are $NOT_DOCED elements not yet documented<br/><br/>There are $NOT_DOC_MEMBER undocumented member elements.<br/>Thefollowing functions have undocumented parameters:<br/>$DOC_PARAM</br>The following errors in dcumentation require fixing:<br/>$DOC_ERROR<br/><br/>$DOC_UNSUPPORTED</body></html>" > ${HTML_PATH}/api/report.html
 
 # Create and commit the documentation repo.
 cd ${HTML_PATH}
